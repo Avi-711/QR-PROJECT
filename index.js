3145f65d5bea248887f1fadb88bbcee85b013a90
avi-711 <uchiha07112000@gmail.com>
2025-03-06 06:42:25 +0530

    java-script

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
-const  student = {
-    age:18,
-    name:"rahul", 
-    isFollw:"true",
-    offer:5,
-    price:200,
-}
-console.log (student);
+        //  ARITMETIC OPRATOR 
+
+        // ADDITION        
+let addition1 = 10;
+let addition2 = 5;
+
+console.log(addition1+addition2);    
+
+        //   SUBTRACTION
+let subtraction1 = 10;
+let subtraction2 = 5;
+
+console.log(subtraction1-subtraction2);
+
+        //   MULTIPLICATION
+let multiplication1 = 10;
+let multiplication2 = 5;
+
+console.log(multiplication1*multiplication2);
+
+        //    DIVISION
+let division1 = 10;
+let division2 = 5;
+
+console.log(division1/division2);
+
+         //   MODULUS 
+let modulus1 = 10;
+let modulus2 = 5;
+
+console.log(modulus1%modulus2);
+         
+        // EXPONENTIATION
+let exponentiation1 = 10;
+let exponentiation2 = 5;
+
+console.log(exponentiation1**exponentiation2);
+
+//        PRE INCREMENT / DECREMENT
+let preIncrement1 = 10;
+let preDecrement2 = 5;
+
+console.log(++preIncrement1);
+
+console.log(--preDecrement2);
+ 
+console.log(preIncrement1);
+console.log(preDecrement2);
+
+
+
+        //  Post DECREMENT / DECREMENT
+let postIncrement1 = 10;
+let postDecrement2 = 5;
+
+console.log(postIncrement1++);
+
+console.log(postDecrement2--);
+
+console.log(postIncrement1);
+console.log(postDecrement2); 
+
