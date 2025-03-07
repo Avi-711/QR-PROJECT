1d4acda1203966ad0a59ed0ce53757ecf21eb6a3
avi-711 <uchiha07112000@gmail.com>
2025-03-07 08:11:13 +0530

    add and&&or||not!

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
         //  ARITMETIC OPRATOR 
 
         // ADDITION        
-let addition1 = 10;
-let addition2 = 5;
+/*
 
-console.log(addition1+addition2);    
+let addition1 = 10;
+let addition2 = 5; 
+
+console.log(addition1+addition2);  
+
+*/  
 
         //   SUBTRACTION
+/*
+
 let subtraction1 = 10;
 let subtraction2 = 5;
 
-console.log(subtraction1-subtraction2);
+console.log(subtraction1-subtraction2);  
+
+*/
 
         //   MULTIPLICATION
-let multiplication1 = 10;
+ /*
+ 
+ let multiplication1 = 10;
 let multiplication2 = 5;
 
-console.log(multiplication1*multiplication2);
+console.log(multiplication1*multiplication2);  
 
-        //    DIVISION
+*/
+
+        //    DIVISION 
+/*
+
 let division1 = 10;
 let division2 = 5;
 
-console.log(division1/division2);
+console.log(division1/division2);  
+
+*/
 
          //   MODULUS 
-let modulus1 = 10;
+ /*
+ 
+ let modulus1 = 10;
 let modulus2 = 5;
 
-console.log(modulus1%modulus2);
+console.log(modulus1%modulus2);  
+
+*/
          
         // EXPONENTIATION
-let exponentiation1 = 10;
+/*   let exponentiation1 = 10;
 let exponentiation2 = 5;
 
-console.log(exponentiation1**exponentiation2);
+console.log(exponentiation1**exponentiation2);  */
 
 //        PRE INCREMENT / DECREMENT
+
+/* 
+
 let preIncrement1 = 10;
 let preDecrement2 = 5;
 
@@ -45,11 +68,13 @@ console.log(++preIncrement1);
 console.log(--preDecrement2);
  
 console.log(preIncrement1);
-console.log(preDecrement2);
+console.log(preDecrement2);  
 
+*/
 
+        //  Post INCREMENT / DECREMENT
+/*  
 
-        //  Post DECREMENT / DECREMENT
 let postIncrement1 = 10;
 let postDecrement2 = 5;
 
@@ -59,4 +84,130 @@ console.log(postDecrement2--);
 
 console.log(postIncrement1);
 console.log(postDecrement2); 
+
+*/
+
+       
+//    ASSIGMEANT OPERATORS 
+
+ /* 
+
+ let a = 5;
+a += 10;
+console.log(a);
+
+let b = 10;
+b -= 5;
+console.log(b);
+
+let c = 5;
+c *= 5;
+console.log(c); 
+*/ 
+
+//        COMPARISON OPRETORS 
+/*
+
+console.log(10<20);
+
+console.log(29>50);
+
+console.log( 10 >= 5);
+
+console.log( 10 <= 5);
+
+*/
+
+//      strict equality 
+
+/*
+
+let string = "10";
+console.log(string);
+
+let number = 10;
+console.log(number);
+
+console.log(string == number);
+
+console.log(string === number);
+
+let number1 = 20;
+let number2 = 20;
+
+console.log(number1);
+
+console.log(number2);
+
+console.log(number1 === number2);
+  
+*/
+
+//    TERNARY OPERATORS 
+
+/*
+
+let age = 25;
+let status1 = (age>18) ? "i can vote" : "icanot vote";
+console.log(status1);
+
+let age1 = 10;
+let status2 = (age1>20) ? "i can vote" : "i canot vote";
+console.log(status2);
+
+*/
+
+//    LOGICAL OPERATORS 
+      
+ //   AND &&
+
+/*
+
+let and1 = true;
+let and2 = false;
+let and3 = true;
+
+console.log(and2 && and3);    
+console.log(and1 && and2);
+console.log(and1 && and3);
+
+let num1 = 0 ;
+let num2 = 1 ;
+let num3 = 1 ;
+
+console.log(num1 && num2);    // 0
+console.log(num2 && num3);    // 1
+console.log(num1 && num3);    // 0
+
+*/
+
+//   OR ||  
+
+let or1 = false ;
+let or2 = 3 ;
+let or3 = true ;
+
+console.log (or1 || or2);
+console.log (or2 || or3);
+console.log (or1 || or3);
+
+let numone1 = 0;
+let numtwo2 = 1;
+let numthree = 0;
+
+console.log (numone1 || numtwo2);
+console.log ( numtwo2 || numthree);
+console.log (numone1 || numthree);
+
+//      NOT | 
+
+let alert1 = false ;
+let alert;
+let alert3 = NaN ;
+let mySum = 45;
+let gone = false;
+let isFirst = true;
+
+
+
 
